da256103136961e63da223a5f114a539838f4038
ochanuco <teacatus@gmail.com>
2026-04-05 00:10:00 +0900

    Handle signal cancellation as clean exit

--- cmd/marume/main.go
+++ cmd/marume/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"os"
 	"os/signal"
@@ -15,6 +16,9 @@ func main() {
 	defer stop()
 
 	if err := cli.Run(ctx, os.Args[1:], os.Stdin, os.Stdout, os.Stderr); err != nil {
+		if errors.Is(err, context.Canceled) {
+			os.Exit(0)
+		}
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(cli.ExitCode(err))
 	}
